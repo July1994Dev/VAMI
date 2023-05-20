c6bba88122dfd8bf37b6462a980e745d04f7839f
julioacevedo01 <juliotoluca.1994@gmail.com>
2023-05-20 12:29:39 -0600

    test

--- src/main.js
+++ src/main.js
@@ -1,3 +1,9 @@
+import "../src/assets/lib/easing/easing.min.js";
+import "../src/assets/lib/waypoints/waypoints.min.js";
+import "../src/assets/lib/counterup/counterup.min.js";
+import "../src/assets/lib/owlcarousel/owl.carousel.min.js";
+import "../src/assets/js/main.js";
+
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import App from './App.vue';

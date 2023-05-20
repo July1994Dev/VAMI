2db32eaa9c4bd944f9a466a9425ad76a0ae5fc48
julioacevedo01 <juliotoluca.1994@gmail.com>
2023-05-20 12:36:38 -0600

    fix

--- src/components/pages/InicioComponent.vue
+++ src/components/pages/InicioComponent.vue
@@ -3,7 +3,7 @@ import ContactDataComponent from '../header/ContactDataComponent.vue';
 import NavigationComponent from '../header/NavigationComponent.vue';
 import AboutComponent from '../about/AboutComponent.vue';
 import FactsComponent from '../facts/FactsComponent.vue';
-import FeaaturesComponent from '../features/FeaturesComponent.vue';
+import FeaturesComponent from '../features/FeaturesComponent.vue';
 import ServicesComponent from '../services/ServicesComponent.vue';
 import PricesComponent from '../pricing/PricingComponent.vue';
 import QuotesComponent from '../quotes/QuotesComponent.vue';
